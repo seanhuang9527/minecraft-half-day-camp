7f1388efa59384b4c4764a597992238b400c710d
Sean Huang <seanhuang0407@gmail.com>
2026-08-22 08:35:57 +0800

    Standardize batch K bridge lesson

---
+++ batch-k-nested-loop-bridge/code/high.py
@@ -0,0 +1,10 @@
+def on_on_chat():
+    agent.move(UP, 1)
+    for index in range(50):
+        for index2 in range(4):
+            agent.place(DOWN)
+            agent.move(RIGHT, 1)
+        agent.move(LEFT, 4)
+        agent.move(FORWARD, 1)
+        agent.move(DOWN, 1)
+player.on_chat("4", on_on_chat)
