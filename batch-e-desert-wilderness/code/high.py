d9e626df9eac308a1cb6876c1fdfdb916022da27
Sean Huang <seanhuang0407@gmail.com>
2026-08-22 08:08:00 +0800

    docs: standardize batch E lesson plan

--- batch-e-desert-wilderness/code/high.py
+++ batch-e-desert-wilderness/code/high.py
@@ -1,2 +1,10 @@
-# 目前缺少
-# 此梯次尚未找到第三個高階程式，待後續討論後補上。
+def on_on_chat():
+    agent.set_slot(1)
+    agent.move(UP, 1)
+    for index in range(12):
+        for index2 in range(3):
+            agent.place(DOWN)
+            agent.move(RIGHT, 1)
+        agent.move(LEFT, 3)
+        agent.move(FORWARD, 1)
+player.on_chat("3", on_on_chat)
